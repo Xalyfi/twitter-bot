0ed6f2a8ea9f5a1cf9d9dc2929e3725129f35c75
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2022-10-02 08:41:29 +0000

    バグ解消

--- routes/follow.js
+++ routes/follow.js
@@ -1,33 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const TwitterStrategy = require('passport-twitter');
 const { TwitterApi } = require('twitter-api-v2');
-
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-  
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
-// passport-twitterの初期化
-passport.use(new TwitterStrategy({
-        consumerKey: require('../config.json').consumer_key,//TwitterのconsumerKey
-        consumerSecret: require('../config.json').consumer_secret,//TwitterのconsumerSecret
-        callbackURL: require('../config.json').BASE_URL+'/followers'//認証成功時の戻り先URL
-    },
-    async function(token, tokenSecret, profile, done) {
-        // 認証が完了したtwitterIdを検証する
-        // 例えばtwitteridがDBの中に存在するかということを確認する
-        // 検証結果によってdoneの書き方を以下のように指定する
-        //     検証成功 : return done(null,profile);
-        //     検証失敗 : return done(null,false);
-        //     例外発生 : return done(null);
-        return done(null,profile);
-    }
-));
 
 //ユーザーのフォロワーを取得する
 router.get('/',
@@ -35,11 +8,10 @@ router.get('/',
         const api = new TwitterApi({
             appKey: require('../config.json').consumer_key,
             appSecret: require('../config.json').consumer_secret,
-            accessToken: '',
-            accessSecret: '',
+            accessToken: require('../config.json').access_token,
+            accessSecret: require('../config.json').access_token_secret,
         });
-        const follows = await api.v2.following('1554780761173426176',{"user.fields":'name,profile_image_url,description'});
-        console.log(follows);
+        const follows = await api.v2.following(req.user.id,{"user.fields":'name,profile_image_url,description'});
         res.render('follows',{follows:follows});
 });
 
--- routes/followers.js
+++ routes/followers.js
@@ -1,49 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const TwitterStrategy = require('passport-twitter');
 const { TwitterApi } = require('twitter-api-v2');
 
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-  
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
-// passport-twitterの初期化
-passport.use(new TwitterStrategy({
-        consumerKey: require('../config.json').consumer_key,//TwitterのconsumerKey
-        consumerSecret: require('../config.json').consumer_secret,//TwitterのconsumerSecret
-        callbackURL: require('../config.json').BASE_URL+'/followers'//認証成功時の戻り先URL
-    },
-    async function(token, tokenSecret, profile, done) {
-        // 認証が完了したtwitterIdを検証する
-        // 例えばtwitteridがDBの中に存在するかということを確認する
-        // 検証結果によってdoneの書き方を以下のように指定する
-        //     検証成功 : return done(null,profile);
-        //     検証失敗 : return done(null,false);
-        //     例外発生 : return done(null);
-        return done(null,profile);
-    }
-));
-
-router.get('/',
-    passport.authenticate('twitter'));
 
 //認証した後のリダイレクト先を/followersに設定する
-router.get('/auth', passport.authenticate('twitter', { failureRedirect: '/' }),
+router.get('/',
     async function(req, res) {
-
         const api = new TwitterApi({
             appKey: require('../config.json').consumer_key,
             appSecret: require('../config.json').consumer_secret,
-            accessToken: '',
-            accessSecret: '',
+            accessToken: require('../config.json').access_token,
+            accessSecret: require('../config.json').access_token_secret,
         });
-        const follower = await api.v2.followers(req.user.userid,{"user.fields":'name,profile_image_url,description'});
-        console.log(follower);
+        const follower = await api.v2.followers(req.user.id,{"user.fields":'name,profile_image_url,description'});
         res.render('followers',{followers:follower});
 });
 
