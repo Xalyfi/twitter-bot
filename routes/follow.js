593020f3c989995e6a2dc9e044a71eb951e2088e
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2022-10-06 14:38:42 +0000

    cssを改善した

--- routes/follow.js
+++ routes/follow.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { TwitterApi } = require('twitter-api-v2');
 
 //ユーザーのフォロワーを取得する
-router.get('/',
-    async function(req,res){
+router.get('/',async function(req,res){
+    if (req.user) {
         const api = new TwitterApi({
             appKey: require('../config.json').consumer_key,
             appSecret: require('../config.json').consumer_secret,
@@ -12,8 +12,11 @@ router.get('/',
             accessSecret: require('../config.json').access_token_secret,
         });
         const follows = await api.v2.following(req.user.id,{"user.fields":'name,profile_image_url,description','pagination_token':req.query.page});
-        console.log(follows);
         res.render('follows',{follows:follows});
+    } else {
+        //認証されていない場合はloginにリダイレクトする
+        res.status(401).redirect('/login');
+    }
 });
 
 
--- routes/followers.js
+++ routes/followers.js
@@ -4,8 +4,8 @@ const { TwitterApi } = require('twitter-api-v2');
 
 
 //認証した後のリダイレクト先を/followersに設定する
-router.get('/',
-    async function(req, res) {
+router.get('/',async function(req, res) {
+    if (req.user) {
         const api = new TwitterApi({
             appKey: require('../config.json').consumer_key,
             appSecret: require('../config.json').consumer_secret,
@@ -14,6 +14,10 @@ router.get('/',
         });
         const follower = await api.v2.followers(req.user.id,{"user.fields":'name,profile_image_url,description'});
         res.render('followers',{followers:follower});
+    } else {
+        //認証されていない場合はエラーを返す
+        res.status(401).send('Unauthorized');
+    }
 });
 
 
